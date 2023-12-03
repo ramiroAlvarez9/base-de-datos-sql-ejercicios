806e53c4ab6b3bb3b882171d9ce9209e925b4d2c
Ramiro Alvarez <ramiro.al.alvarez@gmail.com>
2023-12-03 01:54:28 -0300

    avanzando en profesores-tpi

--- datos-creacion/profesores-tpi--creacion.sql
+++ datos-creacion/profesores-tpi--creacion.sql
@@ -56,3 +56,38 @@ CREATE TABLE profesor_trabaja_industria(
 -- insert ----
 
 #PARA ESTE EJERCICIO NO SE PROVEEN LOS DATOS DE PRUEBA.
+
+-- Inserciones en la tabla alumno
+INSERT INTO alumno VALUES (1, 'Apellido1', 'Nombre1', 2020, true);
+INSERT INTO alumno VALUES (2, 'Apellido2', 'Nombre2', 2019, false);
+-- Puedes seguir añadiendo más filas según sea necesario.
+
+-- Inserciones en la tabla inscripto
+INSERT INTO inscripto VALUES ('C001', 1);
+INSERT INTO inscripto VALUES ('C002', 2);
+-- Puedes seguir añadiendo más filas según sea necesario.
+
+-- Inserciones en la tabla curso
+INSERT INTO curso VALUES ('C001', 101, 'M001', 'LUN', 1);
+INSERT INTO curso VALUES ('C002', 102, 'M002', 'MAR', 2);
+-- Puedes seguir añadiendo más filas según sea necesario.
+
+-- Inserciones en la tabla profesor
+INSERT INTO profesor VALUES (101, '123456789', 'ApellidoProf1', 'NombreProf1', 'Marca1', 2010, 'Poliza1', 50000);
+INSERT INTO profesor VALUES (102, '987654321', 'ApellidoProf2', 'NombreProf2', 'Marca2', 2012, 'Poliza2', 60000);
+-- Puedes seguir añadiendo más filas según sea necesario.
+
+-- Inserciones en la tabla materia
+INSERT INTO materia VALUES ('M001', 'Bases de Datos', 1);
+INSERT INTO materia VALUES ('M002', 'Otra Materia', 2);
+-- Puedes seguir añadiendo más filas según sea necesario.
+
+-- Inserciones en la tabla puede_dar
+INSERT INTO puede_dar VALUES ('M001', 101);
+INSERT INTO puede_dar VALUES ('M002', 102);
+-- Puedes seguir añadiendo más filas según sea necesario.
+
+-- Inserciones en la tabla profesor_trabaja_industria
+INSERT INTO profesor_trabaja_industria VALUES (101, 70000);
+INSERT INTO profesor_trabaja_industria VALUES (102, 80000);
+-- Puedes seguir añadiendo más filas según sea necesario.
--- tp4-sql/profesores-tpi.sql
+++ tp4-sql/profesores-tpi.sql
@@ -6,8 +6,14 @@
 
     curso <cod_curso: varchar(6) PK, legajo_prof: int, cod_materia: varchar(30), dia: varchar(3), turno: int>
 
-    profesor <legajo_prof: int PK, cuil: varchar(30), apellido: varchar(30), nombre: varchar(30), marca: varchar(20), 
-        aniodeingreso: int, polizaart: varchar(30), salario: int>
+    profesor < legajo_prof: int PK, 
+               cuil: varchar(30), 
+               apellido: varchar(30), 
+               nombre: varchar(30), 
+               marca: varchar(20), 
+               aniodeingreso: int, 
+               polizaart: varchar(30), 
+               salario: int>
 
     materia <cod_materia: varchar(15) PK,materia: varchar(150), semestre: int>
 
@@ -15,34 +21,154 @@
     
     profesor_trabaja_industria <legajo_prof: int PK, sueldo: int>
     
-
-*/
-
-    (b) Listar el sueldo promedio de los profesores de Bases de Datos y que trabajan en la
-    industria.
-    SELECT  p_bd_t_i.legajo_prof, AVG(p_bd_t_i.sueldo) 
-    FROM    (  
-                SELECT * FROM profesor p JOIN curso c ON p.legajo_prof = c.legajo_prof JOIN profesor_trabaja_industria pti ON pti.legajo_prof = c.legajo_prof 
-            
-            ) AS p_bd_t_i
-
-    WHERE materia ILIKE 'bases de datos'
-   
-    GROUP BY p_bd_t_i;
-
-    
     (a) 
     Listar <cod_curso, legajo, apellido, nombre> donde aparezcan aquellos alumnos y
     docentes que participan del curso 2 de la materia Bases de Datos.
 
-    SELECT cod_curso, legajo, apellido, nombre 
-    FROM curso c JOIN materia m ON c.cod_materia = m.cod_materia JOIN inscripto i ON i.cod_curso = c.cod_curso JOIN alumno a ON a.legajo = i.legajo 
-    WHERE cod_curso = 2 AND materia ILIKE 'bases de datos';
-
-
+    (b) Listar el sueldo promedio de los profesores de Bases de Datos y que trabajan en la
+    industria.
+    
     (c) Listar <legajo-prof, apellido, nombre, añodeingreso> que no pueden dar una materia
     del semestre 1.
 
     (d) Listar <legajo-prof> que no trabajen en la industria.
 
-    (e) Considerando que una materia tiene diferentes profesores en una misma materia, listar <cod-materia, salario-promedio> de los profesores de cada materia.
+    (e) Considerando que una materia tiene diferentes profesores en una misma materia, 
+        listar <cod-materia, salario-promedio> de los profesores de cada materia.
+
+*/
+
+a) 
+SELECT 
+    c.cod_curso, a.legajo, a.apellido,a.nombre
+FROM 
+    alumno a 
+    JOIN inscripto i ON i.legajo      = a.legajo 
+    JOIN curso c     ON c.cod_curso   = i.cod_curso
+    JOIN materia m   ON m.cod_materia = c.cod_materia 
+WHERE 
+    m.materia ILIKE 'bases de datos'
+        AND
+    c.cod_curso = 2
+
+UNION 
+
+SELECT 
+    c.cod_curso, a.legajo, a.apellido,a.nombre
+FROM 
+    profesor p  
+    JOIN puede_dar pd ON  p.legajo_prof = pd.legajo_prof 
+    JOIN materia   m  ON  m.cod_materia = pd.cod_materia 
+    JOIN curso     c  ON  m.cod_materia = c.cod_materia 
+WHERE 
+    m.materia ILIKE 'bases de datos'
+        AND
+    c.cod_curso = 2
+
+b) 
+
+SELECT 
+    p.legajo_prof, 
+    AVG(profesor_trabaja_industria.sueldo) AS sueldo_promedio_de_profesores
+FROM 
+    profesor p 
+    JOIN profesor_trabaja_industria profesor_trabaja_industria 
+    ON   profesor_trabaja_industria.legajo_prof = p.legajo_prof
+GROUP BY 
+    p.legajo_prof;
+
+c) 
+
+-- Solucion V1
+SELECT
+    p.legajo_prof, p.apellido, p.nombre, p.nombre
+FROM 
+    profesor p 
+    JOIN puede_dar puede_dar ON puede_dar.legajo_prof = p.legajo_prof
+    JOIN materia m           ON puede_dar.cod_materia = m.cod_materia
+WHERE 
+    m.semestre <> 1 
+
+    EXCEPT 
+
+SELECT
+    p.legajo_prof, p.apellido, p.nombre, p.nombre
+FROM 
+    profesor p 
+    JOIN puede_dar puede_dar ON puede_dar.legajo_prof = p.legajo_prof
+    JOIN materia m           ON puede_dar.cod_materia = m.cod_materia
+WHERE 
+    m.semestre = 1 
+
+-- solucion V2 
+
+SELECT
+    p.legajo_prof, p.apellido, p.nombre, p.nombre
+FROM 
+    profesor p 
+    JOIN puede_dar puede_dar ON puede_dar.legajo_prof = p.legajo_prof
+    JOIN materia m           ON puede_dar.cod_materia = m.cod_materia
+WHERE 
+    m.semestre NOT IN 
+    (
+        SELECT
+            m.semestre 
+        FROM 
+            materia m 
+        WHERE m.semestre = 1
+    );
+
+-- solucion V3 
+SELECT 
+    p.legajo_prof, p.apellido, p.nombre, p.anioDeIngreso AS anioDeIngreso
+FROM 
+    PROFESOR p 
+    JOIN PUEDE_DAR pdar ON p.legajo_prof = pdar.legajo_prof
+WHERE 
+    p.legajo_prof NOT IN (
+                            SELECT p1.legajo_prof
+                            FROM PROFESOR p1 
+                            JOIN PUEDE_DAR pdar ON p1.legajo_prof =pdar.legajo_prof
+                            JOIN MATERIA m ON pdar.cod_materia = m.cod_materia
+                            WHERE semestre = 1 );
+
+
+d) 
+SELECT 
+    p.legajo_prof 
+FROM 
+    profesor p 
+WHERE 
+    p.legajo_prof 
+        NOT IN (
+                SELECT 
+                    ptindustria.legajo_prof
+                FROM
+                    profesor_trabaja_industria ptrabajaindustria 
+                )
+    
+-- solucion V2 
+
+SELECT 
+    p.legajo_prof 
+FROM
+    profesor p 
+
+    EXCEPT 
+
+SELECT 
+    pti.legajo_prof 
+FROM
+    profesor_trabaja_industria pti
+        
+
+e) 
+    SELECT 
+        m.cod_materia, 
+        AVG(p.salario)
+    FROM 
+        puede_dar pd 
+        JOIN profesor p ON p.legajo_prof = pd.legajo_prof 
+        JOIN materia  m ON m.cod_materia = pd.cod_materia
+GROUP BY 
+    p.legajo_prof, m.cod_materia;
